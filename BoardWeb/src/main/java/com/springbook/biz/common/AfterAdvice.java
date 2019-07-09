5c88b7c917cd4b17ed0d43ee26b5ec583cace325
testkkj <kimkj1212@naver.com>
2019-07-09 16:13:59 +0900

    almost there

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterAdvice.java
@@ -1,19 +1,20 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
 @Service
-@Aspect // Aspect = Pointcut + Advice
-public class LogAdvice {
+@Aspect
+public class AfterAdvice {
 	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
 	public void allPointcut() {
 	}
 
-	@Before("allPointcut()")
-	public void printLog() {
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	@After("allPointcut()")
+	public void finallyLog() {
+		System.out.println("[사후 처리] 비즈니스 로직 수행 후 무조건 동작");
 	}
+
 }
--- BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,8 +1,27 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.user.UserVO;
+
+@Service
+@Aspect
 public class AfterReturningAdvice {
-	public void afterLog() {
-		System.out.println("[사후 처리] 비즈니스 로직 수행 전 동작");
+
+	@AfterReturning(pointcut = "PointcutCommon.getPointcut()", returning = "returnObj")
+	public void afterLog(JoinPoint jp, Object returnObj) {
+		String method = jp.getSignature().getName();
+
+		if (returnObj instanceof UserVO) {
+			UserVO user = (UserVO) returnObj;
+			if (user.getRole().equals("Admin")) {
+				System.out.println(user.getName() + " 로그인(Admin)");
+			}
+		}
+		System.out.println("[사후 처리] " + method + "() 메소드 리턴값 : " + returnObj.toString());
 	}
 
 }
