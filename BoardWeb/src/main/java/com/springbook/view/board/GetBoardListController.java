a18abe87aa55831c74adc279c20da06a72e7bb3d
testkkj <kimkj1212@naver.com>
2019-07-16 14:12:42 +0900

    Spring mvc 적용 버전 커밋

--- BoardWeb/src/main/java/com/springbook/view/board/GetBoardListController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -4,16 +4,17 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardListController implements Controller {
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 목록 검색 처리");
 
 		// 1. 사용자 입력 정보 추출(검색 기능은 나중에 구현)
@@ -22,10 +23,11 @@ public class GetBoardListController implements Controller {
 		BoardDAO boardDAO = new BoardDAO();
 		List<BoardVO> boardList = boardDAO.getBoardList(vo);
 
-		// 3. 검색 결과를 세션에 저장하고 목록 화면을 리턴한다.
-		HttpSession session = request.getSession();
-		session.setAttribute("boardList", boardList);
-		return "getBoardList";
+		// 3. 검색 결과와 화면 정보를 ModelAndView에 저장하여 리턴한다.
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("boardList", boardList); // Model 정보 저장
+		mav.setViewName("getBoardList");
+		return mav;
 	}
 
 }
